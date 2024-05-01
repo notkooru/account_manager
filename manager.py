c01c4ac9f40983a96663aff1efb5f375c31d6e5e
notkooru <notkooru@gmail.com>
2024-04-30 19:53:50 -0600

    Created class User

---
+++ manager.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, username, password, description) -> None:
+        self.username = username
+        self.password = password
+        self.description = description
