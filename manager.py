c90a8fd8aad863b8d036a434194ae57b156c61e2
notkooru <notkooru@gmail.com>
2024-04-30 20:20:53 -0600

    Updated User class to make description optional when creating an object

--- manager.py
+++ manager.py
@@ -2,7 +2,7 @@ import json
 
 # Classes
 class User:
-    def __init__(self, username, password, description) -> None:
+    def __init__(self, username, password, description = '') -> None:
         self.username = username
         self.password = password
         self.description = description
